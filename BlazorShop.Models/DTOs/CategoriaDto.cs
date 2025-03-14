4a32300b49a2b450b8d0922348b101e9eb7de97b
Lucas Pires Kamegawa <126916359+Kamegawaa@users.noreply.github.com>
2025-03-14 17:11:09 -0300

    Delete BlazorShop.Models directory

--- BlazorShop.Models/DTOs/CategoriaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorShop.Models.DTOs
-{
-    public class CategoriaDto
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; } = string.Empty;
-        public string IconCSS { get; set; } = string.Empty;
-    }
-}
